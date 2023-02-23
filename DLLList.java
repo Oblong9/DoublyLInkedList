22d179b35db361e1feb53cf4782f79b0c2df0f01
Oblong9 <98433294+Oblong9@users.noreply.github.com>
2023-02-23 18:44:21 -0500

    Add files via upload

--- DLLList.java
+++ DLLList.java
@@ -10,7 +10,7 @@ public class DLLList<E> implements ListInterface<E>
     protected DLLNode<E> location;
     protected DLLNode<E> next;
     protected DLLNode<E> previous;
-
+    protected boolean found;
     protected int numElements;
 
     @Override
@@ -24,41 +24,80 @@ public class DLLList<E> implements ListInterface<E>
         return false;
     }
 
-    @Override
-    public int size() {
+    // START BELOW
+    // Helper class for searching list
+    protected void find(E target) {
+        found = false;
+        previous = null;
+        next = null;
+        location = head;
 
-        return 0;
+        while(location != null) {
+            if (location.getData().equals(target)) {
+                found = true;
+                break;
+            } else {
+                previous = location;
+                location = location.getNext();
+                next = location.getNext();
+            }
+        }
     }
 
     @Override
+    public int size() {
+        return numElements;
+    }
+ 
+    @Override
     public boolean isEmpty() {
-        
-        return false;
+        return head == null;
     }
 
     @Override
     public boolean contains(E element) {
-        
-        return false;
+        find(element);
+        return found;
     }
 
     @Override
     public E get(E element) {
-        
-        return null;
+        find(element);
+        if(found) {
+            return location.getData();
+        } else {
+            return null;
+        }
     }
 
     @Override
     public E get(int index) {
-        
-        return null;
+        if (index < 0 || index > numElements - 1) {
+            return null;
+        } 
+        else {
+            DLLNode<E> ptr = head;
+
+            for (int i = 0; i < index; i++) {
+                ptr = ptr.getNext();
+            }
+
+            return ptr.getData();
+        }
     }
 
     @Override
     public String toString()
     {
-        
-        return null;
+        StringBuilder listStr = new StringBuilder("\n----------------------\n");
+        DLLNode<E> ptr = head;
+
+        while (ptr != null) {
+            listStr.append(ptr.getData() + "\n");
+            ptr = ptr.getNext();
+        }
+
+        return listStr.toString();
     }
 
 }
